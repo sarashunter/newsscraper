6653f0efeedea80fbc5803bc901a9c576132c7ae
Sara Hunter <34786124+sarashunter@users.noreply.github.com>
2018-11-04 17:16:39 -0500

    Adding initial Article Model

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var cheerio = require("cheerio");
+var axios = require("axios");
+
+var db = require("./models");
+
+
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 mongoose.connect(MONGODB_URI);
