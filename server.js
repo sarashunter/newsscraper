a176af36ed0230d09c81e01680c026608ea88c25
Sara Hunter <34786124+sarashunter@users.noreply.github.com>
2018-11-05 18:23:55 -0500

    Scraping titles works

--- server.js
+++ server.js
@@ -4,9 +4,53 @@ var mongoose = require("mongoose");
 var cheerio = require("cheerio");
 var axios = require("axios");
 
+var app = express();
+
 var db = require("./models");
 
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 mongoose.connect(MONGODB_URI);
+
+app.get("/scrape", function (req, res) {
+    axios.get("https://vox.com/").then(function (response) {
+        var $ = cheerio.load(response.data);
+        var results = [];
+
+        $("h2").children().attr("data-analytics-link", "article").each(function (i, element) {
+
+            // var everything = $(element).children().text();
+
+            var result = {};
+
+            result.title = $(element).text();
+
+            // var title = $(element).text()
+            // console.log(title);
+            // results.push({
+            //     title: title
+            // });
+            // db.scrapedData.insert({ title: title });
+
+            db.Article.create(result)
+                .then(function (dbArticle) {
+                    // View the added result in the console
+                    console.log(dbArticle);
+                    results.push(result);
+                })
+                .catch(function (err) {
+                    // If an error occurred, send it to the client
+                    return res.json(err);
+                });
+
+        });
+        res.json(results);
+
+
+    });
+})
+
+app.listen(3000, function () {
+    console.log("App running on port 3000!");
+});
