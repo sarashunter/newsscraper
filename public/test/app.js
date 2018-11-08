194a309c424395f643979dbda30f66c57fc0b6b1
Sara Hunter <34786124+sarashunter@users.noreply.github.com>
2018-11-08 11:46:32 -0500

    Fixing delete function to remove comment from page

--- public/test/app.js
+++ public/test/app.js
@@ -2,7 +2,7 @@ loadComments = (id) => {
     console.log("none");
 }
 
-$("#submit").on("click", () =>{
+$("#submit").on("click", () => {
     event.preventDefault();
 
     $.ajax({
@@ -13,22 +13,24 @@ $("#submit").on("click", () =>{
             commentText: $("#comment").val(),
             id: $("#submit").attr("data-id")
         }
-    }).then((data) =>{
+    }).then((data) => {
         loadComments($("#submit").attr("data-id"));
     })
 })
 
-$(".delete").click(function() {
+$(".delete").click(function () {
     event.preventDefault();
-    
+    const commentID = $(this).attr("data-commentId");
+    console.log("in here?");
+    $("#" + commentID).remove();
+
     $.ajax({
         type: "POST",
         url: "/delete",
         dataType: "json",
-        data: {id: $(this).attr("data-id"),
-        commentId: $(this).attr("data-commentId")
-    }
-    }).then(() =>{
-        loadComments($("#submit").attr("data-id"))
-    })
+        data: {
+            id: $(this).attr("data-id"),
+            commentId: $(this).attr("data-commentId")
+        }
+    });
 })
