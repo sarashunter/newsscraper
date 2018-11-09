020f99ae0f3d1216f79443f1dbc458baf127ddb1
Sara Hunter <34786124+sarashunter@users.noreply.github.com>
2018-11-08 20:51:08 -0500

    Adding comments

--- public/assets/app.js
+++ public/assets/app.js
@@ -1,10 +1,5 @@
-loadComments = (id) => {
-    console.log("none");
-}
-
 $("#submit").on("click", () => {
     event.preventDefault();
-
 
     $.ajax({
         type: "POST",
@@ -19,17 +14,17 @@ $("#submit").on("click", () => {
         const id = data.comments.slice(-1);
         var newComment = $("<div>");
         newComment.attr("id", id);
-        newComment.html( `<button  type="button" class="btn btn-light delete" data-id="${id}" data-commentId="${id}"><img src="/assets/open-iconic-master/svg/trash.svg" alt="icon name"></button>` + $("#comment").val());
-    
+        newComment.html(`<button  type="button" class="btn btn-light delete" data-id="${id}" data-commentId="${id}"><img src="/assets/open-iconic-master/svg/trash.svg" alt="icon name"></button>` + $("#comment").val());
+
         $("#comments").append(newComment);
     })
 })
 
-
-    $(document).on('click', '.delete', function() {
+//When delete is clicked, the comment is removed from the page and then a POST request updates it in mongoDB.
+$(document).on('click', '.delete', function () {
     event.preventDefault();
     const commentID = $(this).attr("data-commentId");
-    console.log("in here?");
+
     $("#" + commentID).remove();
 
     $.ajax({
